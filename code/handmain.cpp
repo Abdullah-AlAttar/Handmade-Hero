51a122300e74a13f94dd21abe6e1f17620acda67
Abdullah-AlAttar <abdullah.mohammad.alattar@gmail.com>
2019-11-16 12:55:02 +0400

    Finished Day2, window is open

--- code/handmain.cpp
+++ code/handmain.cpp
@@ -1,27 +1,118 @@
 #include <iostream>
 
-#include <windows.h>
 #include <string>
+#include <windows.h>
+#include <random>
+#define IGNORE_UNUSED(x) (void)(x)
 
-struct Point
+LRESULT CALLBACK MainWindowCallback(HWND window,
+                                    UINT msg,
+                                    WPARAM wparam,
+                                    LPARAM lparam)
 {
-  int a,b;
-};
-int CALLBACK WinMain(
-    HINSTANCE hInstance,
-    HINSTANCE hPrevInstance,
-    LPSTR lpCmdLine,
-    int nShowCmd)
-{
-  auto [a,b] = Point{3,4};
-  std::string str{"Wow"};
-  str += std::to_string(a) + " "+ std::to_string(b);
+    LRESULT result = 0;
+    switch (msg)
+    {
+    case WM_SIZE:
+    {
+        OutputDebugStringA("WM_SIZE\n");
+    }
+    break;
+    case WM_DESTROY:
+    {
+        OutputDebugStringA("WM_DESTORY\n");
+    }
+    break;
+    case WM_CLOSE:
+    {
+        OutputDebugStringA("WM_CLOSE\n");
+    }
+    break;
+    case WM_ACTIVATEAPP:
+    {
+        OutputDebugStringA("WM_ACTIVATEAPP\n");
+    }
+    break;
+    case WM_PAINT:
+    {
+        PAINTSTRUCT paint;
+        HDC device_context = BeginPaint(window, &paint);
+        PatBlt(device_context,
+               paint.rcPaint.left,
+               paint.rcPaint.top,
+               paint.rcPaint.right - paint.rcPaint.left,
+               paint.rcPaint.bottom - paint.rcPaint.top,
+               rand()%2 ? WHITENESS : BLACKNESS);
 
-  std::cout<<"Hello World"<<std::endl;
-  MessageBox(
-      0,
-      str.c_str(),
-      "HandMade",
-      MB_OK | MB_ICONINFORMATION);
-  return 0;
+        EndPaint(window, &paint);
+    }
+    default:
+    {
+        result = DefWindowProc(window, msg, wparam, lparam);
+        // OutputDebugStringA("default\n");
+    }
+    break;
+    }
+    return result;
+}
+
+int WINAPI wWinMain(_In_ HINSTANCE hInstance,
+                    _In_opt_ HINSTANCE hPrevInstance,
+                    _In_ LPWSTR lpCmdLine,
+                    _In_ int nShowCmd)
+{
+    IGNORE_UNUSED(hPrevInstance);
+    IGNORE_UNUSED(lpCmdLine);
+    IGNORE_UNUSED(nShowCmd);
+
+    WNDCLASS win_class = {};
+
+    win_class.style       = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+    win_class.lpfnWndProc = MainWindowCallback;
+    win_class.hInstance   = hInstance;
+    // win_class.hIcon
+    // win_class.hbrBackground
+    win_class.lpszClassName = L"HandmadeHeroWindowClass";
+
+    if (RegisterClass(&win_class))
+    {
+        HWND window_handle = CreateWindowEx(0,
+                                            win_class.lpszClassName,
+                                            L"Handmade Hero",
+                                            WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+                                            CW_USEDEFAULT,
+                                            CW_USEDEFAULT,
+                                            CW_USEDEFAULT,
+                                            CW_USEDEFAULT,
+                                            NULL,
+                                            NULL,
+                                            hInstance,
+                                            NULL);
+        if (window_handle)
+        {
+            ShowWindow(window_handle, SW_SHOWDEFAULT);
+            MSG message;
+            for (;;)
+            {
+                BOOL msg_res = GetMessage(&message, 0, 0, 0);
+                if (msg_res > 0)
+                {
+                    TranslateMessage(&message);
+                    DispatchMessage(&message);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        else
+        {
+        }
+    }
+    else
+    {
+        // TODO: LOG
+    }
+    return 0;
 }
